fad862f7d5209fbc3a523752ebc9320e12a75bbd
Hans <hansandika70@gmail.com>
2021-11-28 02:38:05 +0700

    use errorlog

--- config/logging.php
+++ config/logging.php
@@ -42,9 +42,9 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
+            'driver' => 'errorlog',
             'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'level' => 'debug',
         ],
 
         'daily' => [
